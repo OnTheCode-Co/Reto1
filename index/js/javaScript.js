9a40f30810d34bf8de98a415c503b0b5e475a207
iñaki <ikitess@gmail.com>
2019-10-07 12:35:52 +0200

    icCambios index y js tiaportal

--- index/js/javaScript.js
+++ index/js/javaScript.js
@@ -40,7 +40,7 @@ window.onload = function () {
         radioCotas.addEventListener("change", function () {
             console.log("Radio cotas cambia de valor");
             deshabilitar($("#slider_parada"), btnParada, inputParada);
-            habilitar($("#slider_cota"), btnCota, inputCota);
+            habilitar($("#slider_cota"), btnCota, inputCota,radioParadas);
         });
         // Esto lo hace una vez para seleccionar el r-button de cotas cuando carga la página
         radioCotas.dispatchEvent(new Event("change"));
@@ -48,7 +48,7 @@ window.onload = function () {
         radioParadas.addEventListener("change", function () {
             console.log("Radio paradas cambia de valor");
             deshabilitar($("#slider_cota"), btnCota, inputCota);
-            habilitar($("#slider_parada"), btnParada, inputParada);
+            habilitar($("#slider_parada"), btnParada, inputParada,radioCotas);
         });
         /* ---------------------------------------------------------------------------------------------------------- */
 
@@ -253,11 +253,12 @@ window.onload = function () {
          * @param boton
          * @param input
          */
-        function habilitar(slider, boton, input) {
+        function habilitar(slider, boton, input,rButtonElOtro) {
             input.readOnly = false;
             boton.disabled = false;
             boton.style.backgroundColor = "white";
             slider.slider('enable');
+            rButtonElOtro.checked = false;
         }
 
         /**
@@ -266,7 +267,7 @@ window.onload = function () {
          * @param boton
          * @param input
          */
-        function deshabilitar(slider, boton, input) {
+        function deshabilitar(slider, boton, input,rButton) {
             input.readOnly = true;
             boton.disabled = true;
             boton.style.backgroundColor = "E7E6E6";
